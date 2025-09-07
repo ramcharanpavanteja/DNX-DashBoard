25a5207c8a7cbc1d7b932e8b17645a9b6c8c24df
ramcharanpavanteja <pramcharan2004.com>
2025-09-07 23:01:03 +0530

    api set changes also

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -155,7 +155,7 @@ function Layout() {
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/mern-dnx-dashboard">
+    <BrowserRouter basename="/">
       <Layout />
     </BrowserRouter>
   </React.StrictMode>
