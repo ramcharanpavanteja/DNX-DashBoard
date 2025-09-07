1783fd0f975a3351da736a23986e875d68eb2619
ramcharanpavanteja <pramcharan2004.com>
2025-09-07 21:58:38 +0530

    moved desktop

--- client/src/assets/icons/Desktop/javascript/hello.js
+++
@@ -1,9 +0,0 @@
-let passphrase="hi iam ram charn";
-console.log(passphrase.length)
-let fourwords=passphrase.split(" ");
-if(fourwords.length>3){
-console.log("four words"+fourwords);
-}
-else{
-    console.log("not matched");
-}
