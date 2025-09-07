8c702126d60fa5143ad511c74584c18f8430aad1
ramcharanpavanteja <pramcharan2004.com>
2025-09-07 20:05:38 +0530

    changed package.json files

--- server/src/index.js
+++ server/src/index.js
@@ -34,7 +34,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // --- Serve React frontend build ---
-app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
